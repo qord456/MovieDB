fba99d6259bea627cea7c76f8c310fb57316047b
Syaeful Hidayah <syaeful.hidayah123@gmail.com>
2023-08-18 13:41:48 +0700

    [FEATURE] add modal popup feature fro detail movies

--- App.tsx
+++ App.tsx
@@ -5,22 +5,20 @@
  * @format
  */
 
-import React from 'react';
-import {ScrollView} from 'react-native';
-import TopHeader from './src/components/TopHeader';
-import Styles from './src/utils/Styles';
-import Genres from './src/components/Genres';
+import React from "react";
+import { ScrollView } from "react-native";
+import TopHeader from "./src/components/TopHeader";
+import Styles from "./src/utils/Styles";
+import Genres from "./src/components/Genres";
 // import MovieList from './src/components/MovieList';
-import Movies from './src/components/MovieList copy';
-
+import Movies from "./src/components/MovieList";
 
 function App() {
   return (
     <ScrollView style={Styles.allcoverage}>
       <TopHeader />
       <Genres />
-      {/* <MovieList/> */}
-      <Movies/>
+      <Movies />
     </ScrollView>
   );
 }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,26 +1,74 @@
-import React from "react";
-import { Text, View, Image, ScrollView } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Modal,
+  TouchableOpacity
+} from "react-native";
 import MovieData from "../contents/MovieData";
+import Styles from "../utils/Styles";
 
-const MovieList = () => {
+const Movies = () => {
   const data = MovieData();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
   console.log("Movie List: ini data dari data render------", data);
 
+  const openModal = (movie) => {
+    setSelectedMovie(movie);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setSelectedMovie(null);
+    setModalVisible(false);
+  };
+
   return (
-    <ScrollView>
-      <View>
-        {data.map((data) => (
-          <View key={data.id}>
-            <Text>{data.poster_path}</Text>
-            <Image
-              source={{ uri: "https://image.tmdb.org/t/p/w500" + data.poster_path }}
-              style={{ width: 200, height: 300 }} // Adjust width and height as needed
-            />
-          </View>
-        ))}
-      </View>
+    <ScrollView contentContainerStyle={Styles.container}>
+      {data.map((data) => (
+        <TouchableOpacity
+          key={data.id}
+          style={Styles.movieContainer}
+          onPress={() => openModal(data)}
+        >
+          <Image
+            source={{
+              uri: "https://image.tmdb.org/t/p/w500" + data.poster_path
+            }}
+            style={Styles.image}
+          />
+          <Text style={Styles.textCat2}>{data.title}</Text>
+        </TouchableOpacity>
+      ))}
+
+      <Modal visible={modalVisible} animationType="slide">
+        <View style={Styles.modalContainer}>
+          {selectedMovie && (
+            <View>
+              <Image
+                source={{
+                  uri:
+                    "https://image.tmdb.org/t/p/w500" +
+                    selectedMovie.poster_path
+                }}
+                style={Styles.modalImage}
+              />
+              <Text style={Styles.modalText}>Title: {selectedMovie.title}</Text>
+              <Text style={Styles.modalText}>
+                Overview: {selectedMovie.overview}
+              </Text>
+              <TouchableOpacity style={Styles.closeButton} onPress={closeModal}>
+                <Text style={Styles.closeButtonText}>Close</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </View>
+      </Modal>
     </ScrollView>
   );
 };
 
-export default MovieList;
+export default Movies;
